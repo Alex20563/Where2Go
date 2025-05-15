f731f5172a4e1c0967df49669528e479bb6686aa
Полина <sadekova59@gmail.com>
2025-05-15 16:24:37 +0300

    [bugFix] фикс ошибки после добавления https

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,9 +113,9 @@ DATABASES = {
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://localhost:3000",
+    "https://localhost:8080",
+    "https://localhost:8081",
+    "https://localhost:3000",
 ]
 
 CORS_ALLOW_HEADERS = [
